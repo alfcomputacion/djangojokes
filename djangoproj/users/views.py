8f72060ba2d4add9baef1a6c9f67f74528a2c701
Jose Rosas <alfcomputacion@gmail.com>
2023-01-17 17:42:41 -0800

    change password redirects to my-account

--- djangoproj/users/views.py
+++ djangoproj/users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 
+from allauth.account.views import PasswordChangeView
 # Create your views here.
 from .forms import CustomUserChangeForm
 
@@ -12,3 +14,7 @@ class MyAccountPageView(UpdateView):
 
     def get_object(self):
         return self.request.user
+
+
+class CustomPasswordChangeView(PasswordChangeView):
+    success_url = reverse_lazy('my-account')
